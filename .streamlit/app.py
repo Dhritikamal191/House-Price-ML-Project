b785d4d2500047677debbc3f7608fa31b8d84584
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-06-20 22:38:14 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -71,11 +71,6 @@ st.divider()
 
 st.sidebar.header("🏡 Property Information")
 
-0 == Linear Regression
-1 == Ridge Regression
-2 == Lasso Regression
-3 == Random Forest
-
 selected_model = st.sidebar.selectbox(
     "Select Model",
     list(models.keys()),
