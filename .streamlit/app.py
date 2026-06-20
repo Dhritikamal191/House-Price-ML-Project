811088f00ef3ba5232ed6dfaa282474ebfa8fd50
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-06-20 14:27:37 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -175,7 +175,7 @@ predict = st.button(
 if predict:
 
     prediction = model.predict(input_df)[0]
-    prediction = max(prediction,0)
+    prediction = min(prediction,0)
 
     st.success(
         f"Estimated House Price: ${prediction:,.0f}"
